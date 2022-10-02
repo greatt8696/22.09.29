60d8a525eb2d114114c29443b24034e9b52116c8
greatt8696 <greatt8696@gmail.com>
2022-10-02 22:18:04 +0900

    init

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Count from "./Count";
-import CountView from "./CountView";
-import Report from "./Report";
-import Reports from "./Reports";
-import Details from "./Details";
-
-export { Count, CountView, Report, Reports, Details };
